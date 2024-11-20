ec65a087c77eef746ea18959b430c5d0794b023a
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-20 21:40:27 +0700

    member.c

--- member.c
+++ member.c
@@ -1,1 +1,27 @@
-yr5
+#include <stdio.h>
+#include <string.h>
+#include "member.h"
+
+extern Member member[MAX];
+extern int total_member;
+
+void load_member() {
+    FILE *file = fopen("member.txt", "r");
+    if (file) {
+        while (fscanf(file, "%[^,],%d\n", member[total_member].nama, &member[total_member].poin) != EOF) {
+            total_member++;
+        }
+        fclose(file);
+    }
+}
+
+void save_member() {
+    FILE *file = fopen("member.txt", "w");
+    for (int i = 0; i < total_member; i++) {
+        fprintf(file, "%s,%d\n", member[i].nama, member[i].poin);
+    }
+    fclose(file);
+}
+
+// Other member functions like tambah_member, tambah_poin_member, etc.
+
