1b12393fcd8b852440d5d038f5617a057024b1b0
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-20 21:38:33 +0700

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-dfgftgrdt
+#include <stdio.h>
+#include <stdlib.h>
+#include "barang.h"
+#include "member.h"
+#include "user.h"
+#include "kasir.h"
+
+int main() {
+    load_barang();
+    load_member();
+    load_user();
+    if (!login()) {
+        return 0;
+    }
+    menu();
+    return 0;
+}
+
