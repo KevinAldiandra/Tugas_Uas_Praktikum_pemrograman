20728f6108b4c68694ab9acd06663c301e6f56b5
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-20 21:41:41 +0700

    user.c

--- user.c
+++ user.c
@@ -1,1 +1,18 @@
-rtyt
+#include <stdio.h>
+#include <string.h>
+#include "user.h"
+
+extern User user[MAX];
+extern int total_user;
+
+void load_user() {
+    FILE *file = fopen("user.txt", "r");
+    if (file) {
+        while (fscanf(file, "%[^,],%[^\n]\n", user[total_user].username, user[total_user].password) != EOF) {
+            total_user++;
+        }
+        fclose(file);
+    }
+}
+
+// Other user functions like login
