693c1991b25c2ae285984db69784fb7f2d7b4e0a
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-20 21:46:31 +0700

    kasir.h

--- kasir.h
+++ kasir.h
@@ -1,1 +1,11 @@
+#ifndef KASIR_H
+#define KASIR_H
+
+extern float total_penjualan;
+
+void laporan_penjualan();
+void pembayaran();
+// Declare other kasir functions here like tambah_barang, lihat_barang, etc.
+
+#endif
 
