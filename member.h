7e83bc245a283deea418abe7a3f0d9e2264199a1
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-20 21:40:51 +0700

    member.h

--- member.h
+++ member.h
@@ -1,1 +1,18 @@
-ftr
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#define MAX_NAMA 50
+
+typedef struct {
+    char nama[MAX_NAMA];
+    int poin;
+} Member;
+
+extern Member member[MAX];
+extern int total_member;
+
+void load_member();
+void save_member();
+// Declare other member functions here like tambah_member, tambah_poin_member, etc.
+
+#endif
