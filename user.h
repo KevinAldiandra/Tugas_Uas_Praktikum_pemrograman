1e8915f868c1aaa8bec733b0163ec23c5f906cee
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-20 21:42:14 +0700

    user.h

--- user.h
+++ user.h
@@ -1,1 +1,18 @@
-tyrt
+#ifndef USER_H
+#define USER_H
+
+#define MAX_NAMA 50
+#define MAX_PASSWORD 20
+
+typedef struct {
+    char username[MAX_NAMA];
+    char password[MAX_PASSWORD];
+} User;
+
+extern User user[MAX];
+extern int total_user;
+
+void load_user();
+// Declare other user functions like login here
+
+#endif
