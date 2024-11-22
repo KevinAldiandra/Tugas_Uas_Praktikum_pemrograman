e7eaffbe72e7a842fb54a42d31665492d9b066b9
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2024-11-22 19:48:26 +0700

    member.h

--- member.h
+++ member.h
@@ -1,17 +1,10 @@
 #ifndef MEMBER_H
 #define MEMBER_H
 
-#define MAX_NAMA 50
-
-typedef struct {
-    char nama[MAX_NAMA];
-    int poin;
-} Member;
-
-extern Member member[MAX];
-extern int total_member;
-
-void load_member();
-void save_member();
+// Deklarasi fungsi untuk menu kelola member
+void menuMember();       // Menampilkan menu kelola member
+void tambahMember();     // Menambahkan member baru
+void tambahPoinMember(); // Menambahkan poin ke member
+void lihatMember();      // Melihat daftar member
 
 #endif
