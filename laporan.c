c22b3a0f19199b6f9d9fa7006b2643fafab4487f
cutfarah <cutfarahsalsabila76@gmail.com>
2024-11-22 20:52:54 +0700

    laporan.c

---
+++ laporan.c
@@ -0,0 +1,24 @@
+#include "laporan.h"
+#include <stdio.h>
+
+// Fungsi untuk melihat laporan penjualan
+void lihatLaporan() {
+  FILE *file = fopen("data/laporan.txt", "r");
+  if (file == NULL) {
+    printf("Gagal membuka file.\n");
+    return;
+  }
+
+  char deskripsi[100];
+  float total;
+
+  printf("\n=== Laporan Penjualan ===\n");
+  printf("Deskripsi\t\tTotal\n");
+  printf("---------------------------------\n");
+
+  while (fscanf(file, "%s %f", deskripsi, &total) != EOF) {
+    printf("%-15s %.2f\n", deskripsi, total);
+  }
+
+  fclose(file);
+}
