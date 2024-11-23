7469fc07e4a14cee4e885040f82b789bc352d66a
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-23 17:04:07 +0700

    kategori.h

---
+++ kategori.h
@@ -0,0 +1,11 @@
+#ifndef KATEGORI_H
+#define KATEGORI_H
+
+void menuKategori();
+void tambahKategori();
+void lihatKategori();
+int getKategoriCount(); 
+void getKategoriList(
+    char categories[][50]); 
+
+#endif
