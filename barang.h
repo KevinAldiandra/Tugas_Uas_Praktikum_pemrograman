3086d768df8b8bc6ff21c99010d367f677692bee
KevinAldiandra <kevinaldiandra25@gmail.com>
2024-11-23 16:58:01 +0700

    barang.h

--- barang.h
+++ barang.h
@@ -1,21 +1,12 @@
 #ifndef BARANG_H
 #define BARANG_H
 
-#define MAX 100
-#define MAX_NAMA 50
-#define MAX_PASSWORD 20
-
-typedef struct {
-    char nama[MAX_NAMA];
-    float harga;
-    int stok;
-    char kategori[MAX_NAMA];
-} Barang;
-
-extern Barang barang[MAX];
-extern int total_barang;
-
-void load_barang();
-void save_barang();
+void menuBarang();
+void tambahBarang();
+void hapusBarang();
+void tambahStokBarang();
+void kurangiStokBarang();
+void lihatBarang();
+float getHargaBarang(char *nama);
 
 #endif
